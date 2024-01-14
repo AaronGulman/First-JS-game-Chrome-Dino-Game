374e4bda17afcbc9291e65b07b610e2719619d10
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-01-14 16:11:49 -0500

    Loop the ground element

--- ground.js
+++ ground.js
@@ -1,11 +1,20 @@
-import { incrementCustomProperty } from "./updateCustomProperty.js"
+import { getCustomProperty, incrementCustomProperty, setCustomProperty } from "./updateCustomProperty.js"
 
 const SPEED = .05
 const ground = document.querySelectorAll('[data-ground]')
 
+export function setupGround(){
+	setCustomProperty(ground[0], "--left",0)
+	setCustomProperty(ground[1], "--left",300)
+
+}
 
 export function updateGround(delta){
 	ground.forEach(ground =>{
 		incrementCustomProperty(ground, "--left", delta * SPEED * -1)
+
+		if(getCustomProperty(ground, '--left') <= -300){
+			incrementCustomProperty(ground, "--left",600)
+		}
 	})
 }
